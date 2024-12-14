dcb14275b18e6c93b47c06d4705a31e239b8e73e
MattiaDV <mattiadv25105@gmail.com>
2024-12-14 15:13:13 +0100

    Reset password

---
+++ script/home.js
@@ -0,0 +1,26 @@
+let lists = document.getElementsByClassName('mini');
+let menu = document.getElementById('menu');
+
+for (let a of lists) {
+    a.addEventListener('click', function() {
+        menu.removeAttribute('open');
+    })
+}
+
+function getCookieValue(cookieName) {
+    const cookies = document.cookie.split("; ");
+    for (let cookie of cookies) {
+        const [name, value] = cookie.split("=");
+        if (name === cookieName) {
+            return value;
+        }
+    }
+    return null; 
+}
+
+const email = getCookieValue("email");
+let infoPersonal = document.getElementById('emailPersonal');
+infoPersonal.innerHTML = "Personal email: " + email;
+
+let em = document.getElementById('email');
+em.value = email;
