05b2bb3e5894cdba2ad7363b55a17115d101e569
MattiaDV <mattiadv25105@gmail.com>
2024-12-21 16:40:20 +0100

    Server initialization

--- api/getPages.js
+++ api/getPages.js
@@ -14,3 +14,78 @@ exports.getIndex = async function(req, res) {
         }
       }
 }
+
+exports.getForgetPasw = async function(req, res) {
+    try {
+        let htmlContent = await readFile('forgetPasw.html', 'utf-8');
+        res.writeHead(200, {'content-type': 'text/html'});
+        res.end(htmlContent);
+        return;
+      } catch(err) {
+        if (err) {
+          console.log(err);
+          res.send("Errore nel server interno");
+          return;
+        }
+      }
+}
+
+exports.getHome = async function(req, res) {
+    try {
+        let htmlContent = await readFile('home.html', 'utf-8');
+        res.writeHead(200, {'content-type': 'text/html'});
+        res.end(htmlContent);
+        return;
+      } catch(err) {
+        if (err) {
+          console.log(err);
+          res.send("Errore nel server interno");
+          return;
+        }
+      }
+}
+
+exports.getLogin = async function(req, res) {
+    try {
+        let htmlContent = await readFile('login.html', 'utf-8');
+        res.writeHead(200, {'content-type': 'text/html'});
+        res.end(htmlContent);
+        return;
+      } catch(err) {
+        if (err) {
+          console.log(err);
+          res.send("Errore nel server interno");
+          return;
+        }
+      }
+}
+
+exports.getPersonalData = async function(req, res) {
+    try {
+        let htmlContent = await readFile('personalData.html', 'utf-8');
+        res.writeHead(200, {'content-type': 'text/html'});
+        res.end(htmlContent);
+        return;
+      } catch(err) {
+        if (err) {
+          console.log(err);
+          res.send("Errore nel server interno");
+          return;
+        }
+      }
+}
+
+exports.getReg = async function(req, res) {
+    try {
+        let htmlContent = await readFile('registrazione.html', 'utf-8');
+        res.writeHead(200, {'content-type': 'text/html'});
+        res.end(htmlContent);
+        return;
+      } catch(err) {
+        if (err) {
+          console.log(err);
+          res.send("Errore nel server interno");
+          return;
+        }
+      }
+}
--- server.js
+++ server.js
@@ -1,14 +1,39 @@
 const express = require('express');
-const {readFile} = require('fs/promises'); 
 const getPages = require('./api/getPages');
 
 const app = express();
 
-app.get('/' || '/index', async (req, res) => {
+app.use(express.static('./'));
+
+app.get('/', async (req, res) => {
   getPages.getIndex(req, res);
 })
 
-app.listen(3000, '127.0.0.1', function(err) {
+app.get('/index', async (req, res) => {
+  getPages.getIndex(req, res);
+})
+
+app.get('/forgetPassword', async (req, res) => {
+  getPages.getForgetPasw(req, res);
+})
+
+app.get('/home', async (req, res) => {
+  getPages.getHome(req, res);
+})
+
+app.get('/login', async (req, res) => {
+  getPages.getLogin(req, res);
+})
+
+app.get('/personalData', async (req, res) => {
+  getPages.getPersonalData(req, res);
+})
+
+app.get('/registrazione', async (req, res) => {
+  getPages.getReg(req, res);
+})
+
+app.listen(function(err) {
   if (err) {
     console.log("Errore nella connessione del server");
   } else {
